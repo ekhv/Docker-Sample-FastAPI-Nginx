7e5dad6f6fc0b902ea3f447d7811971bd3291d4d
ekhv <e.khryashchev@outlook.com>
2023-07-01 07:27:16 +0300

    test endpoint

--- app/main.py
+++ app/main.py
@@ -1,15 +1,8 @@
-from typing import Optional
-
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World123 234"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+def get_root() -> dict:
+    return {"msg": "Hello, World!"}
